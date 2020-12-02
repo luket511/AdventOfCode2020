c5f7176db257c6401e12ceac7eab3fc7ab6dc329
Luke <lnt200011@gmail.com>
2020-12-02 14:55:41 +0000

    Restructuring

---
+++ commonFunctions.py
@@ -0,0 +1,23 @@
+import csv
+
+def split(inpString,breakChar):
+    output = []
+    currentString = ""
+    for cha in inpString:
+        if cha == breakChar:
+            if currentString != "":
+                output.append(currentString)
+            currentString = ""
+        else:
+            currentString += (cha)
+    if currentString != "":
+        output.append(currentString)
+    return output
+
+def reader(inputFile):
+    output = []
+    with open(inputFile) as file:
+        reader = csv.reader(file,delimiter=',')
+        for row in reader:
+            output.append(row[0])
+    return output

40ea530d499ea6fcd0dae84f229da38ebc6af5a1
Luke <lnt200011@gmail.com>
2020-12-02 15:02:42 +0000

    Changed to use pre-existing method

--- Day_2a.py
+++ Day_2a.py
@@ -1,14 +1,14 @@
-from commonFunctions import split, reader
+from commonFunctions import reader
 
 def formatInput(file):
     inpList = reader(file)
     output = []
     for item in inpList:
         map = {}
-        firstFormat = split(item," ")
+        firstFormat = item.split()
         map['password'] = firstFormat[2]
         map['char'] = firstFormat[1][0]
-        amounts = split(firstFormat[0],"-")
+        amounts = firstFormat[0].split("-")
         map['numOne'] = int(amounts[0])
         map['numTwo'] = int(amounts[1])
         output.append(map)

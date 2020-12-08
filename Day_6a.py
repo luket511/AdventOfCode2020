8c0eec718d21f8895195a46cb36fccca5059c491
Taylor Luke (ETAS/ERS-ENG1) <Luke.Taylor@etas.com>
2020-12-08 12:36:59 +0000

    Added day 6

--- Day_6a.py
+++ Day_6a.py
@@ -1,8 +1,27 @@
-from commonFunctions import reader
-INPUT = "input6test.csv"
+from commonFunctions import reader, NEWLINECHAR
+INPUT = "input6.csv"
+
+def processAnswers(answers):
+    groups = []
+    group = set()
+    for answer in answers:
+        if answer == NEWLINECHAR:
+            groups.append(group)
+            group = set()
+        else:
+            for char in answer:
+                group.add(char)
+    if group != set():
+        groups.append(group)
+
+    total = 0
+    for answers in groups:
+        total += len(answers)
+    
+    print (total)
 
 
 
 
 if __name__ == "__main__":
-    pass
+    processAnswers(reader(INPUT))
--- Day_6b.py
+++ Day_6b.py
@@ -1,8 +1,40 @@
-from commonFunctions import reader
-INPUT = "input6test.csv"
+from commonFunctions import reader, NEWLINECHAR
+INPUT = "input6.csv"
 
+def processAnswers(answers):
+    groups = []
+    group = []
+    for answer in answers:
+        if answer == NEWLINECHAR:
+            groups.append(group)
+            group = []
+        else:
+            group.append(answer)
+    if group != []:
+        groups.append(group)
 
+    totalAnswersCorrect = []
+    for group in groups:
+        answersCorrect = 0
+        # answersCorrect = []
+        groupLen = len(group)
+        answerTally = {}
+        for person in group:
+            for question in person:
+                try:
+                    answerTally[question] += 1
+                except KeyError:
+                    answerTally[question] = 1
+        for question in answerTally.keys():
+            if answerTally[question] == groupLen:
+                answersCorrect += 1
+                # answersCorrect.append(question)
+        totalAnswersCorrect.append(answersCorrect)
+    
+    print (sum(totalAnswersCorrect))
 
+                
+                
 
 if __name__ == "__main__":
-    pass
+    processAnswers(reader(INPUT))

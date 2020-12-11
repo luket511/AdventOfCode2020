951306794c0aec1a3a4e3e9bb9fa9f061f28db40
Luke.Taylor@etas.com <Luke.Taylor@etas.com>
2020-12-11 17:02:22 +0000

    Added Day 9

---
+++ Day_9a.py
@@ -0,0 +1,21 @@
+﻿from commonFunctions import reader
+from Day_1a import findMatchingPairs
+INPUT = "input9.csv"
+
+def findBroken(data,preamble):
+    if preamble >= len(data):
+        return None
+    
+    data = list(map(int,data))
+
+    for i in range(preamble,len(data)):
+        number = data[i]
+        if findMatchingPairs(number,data[i-preamble:i]) is None:
+            return number
+    
+    return None
+    
+
+
+if __name__ == "__main__":
+    print (findBroken(reader(INPUT),25))
---
+++ Day_9b.py
@@ -0,0 +1,28 @@
+﻿from commonFunctions import reader
+from Day_9a import findBroken
+INPUT = "input9.csv"
+
+def findSequence(data,targetNum):
+
+    data = list(map(int,data))
+
+    for i in range(len(data)):
+        continuousTotal = 0
+        continuousSequence = []
+        for num in data[i:]:
+            continuousTotal += num
+            continuousSequence.append(num)
+            if continuousTotal == targetNum:
+                return continuousSequence
+            elif continuousTotal > targetNum:
+                break
+
+
+if __name__ == "__main__":
+    dataSet = reader(INPUT)
+    brokenNum = findBroken(dataSet,25)
+    print ("Broken Number: {0}".format(brokenNum))
+    seq = findSequence(dataSet,brokenNum)
+    seq.sort()
+    print ("Sequence: {0}".format(seq))
+    print ("Biggest and Smallest Sum: {0}".format(seq[0]+seq[len(seq)-1]))
